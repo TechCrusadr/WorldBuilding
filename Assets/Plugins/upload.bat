651a984d9f896f42e09219d8df5620a2cd147862
Justin Chow <jjc2184@caa.columbia.edu>
2020-10-18 00:21:38 -0700

    added poliwag and other xtra creatures

--- Assets/Plugins/upload.bat
+++ Assets/Plugins/upload.bat
@@ -1,2 +1,2 @@
 echo off
-curl -v -b cookie -X PUT -F "space_template[zip]=@D:\Unity Projects\2020 World Building Class\Prototype world\World Building Class prototype\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1575666820520608128.json
+curl -v -b cookie -X PUT -F "space_template[zip]=@D:\UnityProjects\2020_WorldBuildingClass\Prototype_world\template.zip" -F "space_template[game_engine_version]=20194" https://account.altvr.com/api/space_templates/1575666820520608128.json
